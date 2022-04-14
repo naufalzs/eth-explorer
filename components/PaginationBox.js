c51c9904e4a416dbc99b8c8190aa9df9c6ecf8d1
naufalzs <naufal.zufar.s@gmail.com>
2022-04-14 20:40:44 +0700

    done pagination

--- components/PaginationBox.js
+++ components/PaginationBox.js
@@ -10,36 +10,49 @@ export default function PaginationBox({
 
   useEffect(() => {
     if (totalPage && activePage) {
-      if (totalPage < 10 || totalPage - activePage <= 10) {
-        if (totalPage < 10) {
-          const arrPage = [];
-          for (let i = 1; i <= totalPage; i++) {
-            arrPage.push(i);
-          }
-          setShowPage(arrPage);
-        } else {
-          const arrPage = [];
-          for (let i = totalPage - 10; i <= totalPage; i++) {
-            arrPage.push(i);
-          }
-          setShowPage(arrPage);
+      if (totalPage < 10) {
+        const arrPage = [];
+        for (let i = 1; i <= totalPage; i++) {
+          arrPage.push(i);
         }
-      } else {
+        setShowPage(arrPage);
+      } else if (activePage < 4) {
         const arrPage2 = [];
-        for (let i = 0; i <= 3; i++) {
-          arrPage2.push(activePage + i);
+        for (let i = 1; i <= 4; i++) {
+          arrPage2.push(i);
         }
         arrPage2.push("...");
-
         for (let i = 1; i >= 0; i--) {
           arrPage2.push(totalPage - i);
         }
         setShowPage(arrPage2);
+      } else if (activePage >= 4 && activePage <= totalPage - 4) {
+        const arrPage3 = [];
+        for (let i = 1; i <= 2; i++) {
+          arrPage3.push(i);
+        }
+        arrPage3.push("...");
+        for (let i = -1; i <= 1; i++) {
+          arrPage3.push(activePage + i);
+        }
+        arrPage3.push("...");
+        for (let i = 1; i >= 0; i--) {
+          arrPage3.push(totalPage - i);
+        }
+        setShowPage(arrPage3);
+      } else if (activePage > totalPage - 4) {
+        const arrPage4 = [];
+        for (let i = 1; i <= 2; i++) {
+          arrPage4.push(i);
+        }
+        arrPage4.push("...");
+        for (let i = 4; i >= 0; i--) {
+          arrPage4.push(totalPage - i);
+        }
+        setShowPage(arrPage4);
       }
     }
   }, [totalPage, activePage]);
-
-  // console.log(arrPage);
 
   const handleClick = (active) => {
     setActivePage(active);
@@ -56,24 +69,26 @@ export default function PaginationBox({
             : undefined
         }
         className={`flex items-center p-2 rounded-l-md border border-gray-300 bg-white text-sm  ${
-          activePage > 1 ? "hover:bg-gray-100 cursor-pointer text-gray-500" : "bg-gray-100 text-gray-300"
+          activePage > 1
+            ? "hover:bg-gray-100 cursor-pointer text-gray-500"
+            : "bg-gray-100 text-gray-300"
         }`}
       >
         <ChevronLeftIcon className="h-5 w-5" />
       </div>
-      {showPage?.map((num) => {
+      {showPage?.map((num, index) => {
         return (
           <div
-            key={num}
-            onClick={() => {
+            key={index}
+            onClick={num!== "..." ? () => {
               handleClick(num);
-            }}
+            } : undefined}
             className={` ${
               num === activePage
                 ? "bg-indigo-50 border-indigo-500 text-indigo-600"
                 : "bg-white border-gray-300 text-gray-500"
             } flex items-center px-4 py-2 border text-sm  ${
-              num !== "..." && "cursor-pointer hover:bg-gray-50"
+              num !== "..." ? "cursor-pointer hover:bg-gray-50" : "bg-gray-100"
             }`}
           >
             {num}
