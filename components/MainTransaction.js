4e30c573151017bdb1048a4be2d6cb07d0ae6608
naufalzs <naufal.zufar.s@gmail.com>
2022-04-15 13:46:22 +0700

    add search by hash

--- components/MainTransaction.js
+++ components/MainTransaction.js
@@ -15,6 +15,8 @@ export default function MainTransaction() {
 
   const [data, setData] = useState(undefined);
   const [filterTo, setFilterTo] = useState(false);
+  const [hash, setHash] = useState(undefined);
+  const [callApi, setCallApi] = useState(false);
   const indexFix = offset * (activePage - 1);
 
   const CBsetAddress = (dress) => {
@@ -29,6 +31,11 @@ export default function MainTransaction() {
     setActivePage(num);
   };
 
+  const handleHash = (e) => {
+    setHash(e.target.hash.value);
+    e.preventDefault();
+  };
+
   useEffect(() => {
     if (address) {
       const apiKey = process.env.NEXT_PUBLIC_API_KEY;
@@ -38,12 +45,20 @@ export default function MainTransaction() {
         )
         .then((res) => {
           setTotalPage(Math.floor(res.data.result.length / offset) + 1);
+          if (hash && hash.length > 0) {
+            const getRes = res.data.result;
+            const filterHash = getRes.filter((e) => e.hash == hash);
+            setData(filterHash);
+            setTotalPage(1);
+          } else {
+            setCallApi(!callApi);
+          }
         })
         .catch((err) => {
           console.log(err);
         });
     }
-  }, [address, offset]);
+  }, [address, offset, hash]);
 
   useEffect(() => {
     if (address) {
@@ -66,8 +81,7 @@ export default function MainTransaction() {
           console.log(err);
         });
     }
-  }, [address, offset, activePage, filterTo]);
-
+  }, [offset, activePage, filterTo, callApi]);
   return (
     <>
       {!step ? (
@@ -88,15 +102,34 @@ export default function MainTransaction() {
               setActivePage={CBsetActivePage}
             />
           </div>
-          <div className="mb-6 flex justify-between">
+
+          <div className="my-6 flex items-center justify-between">
             <button
               onClick={() => {
                 setFilterTo(!filterTo);
               }}
-              className="w-[350px] px-4 py-2 text-lg rounded-md mt-8 bg-gray-300"
+              className="w-[350px] px-4 py-2 text-lg rounded-md bg-gray-300"
             >
               Only show transaction with To value
             </button>
+            <div className="flex items-center justify-between">
+              <form onSubmit={handleHash} className=" flex">
+                <div className="relative">
+                  <input
+                    type="text"
+                    name="hash"
+                    placeholder="Insert TxHash here"
+                    className="w-[300px] px-4 py-3 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-[180px] px-4 py-2 text-lg rounded-md bg-gray-300"
+                >
+                  Search by Hash
+                </button>
+              </form>
+            </div>
           </div>
           <DataTable transactionData={data} indexFix={indexFix} />
         </div>
